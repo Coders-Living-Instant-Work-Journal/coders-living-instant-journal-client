246b6938ef1ac4137abf279d82d8aef9fad0f678
Eugene Monnier <eugene.monnier@gmail.com>
2020-03-08 17:52:26 -0700

    connected server to MongoDB

--- server/index.js
+++ server/index.js
@@ -1,5 +1,16 @@
 require('dotenv').config()
-const { PORT } = process.env
+const { PORT, MONGODB_URI } = process.env
+const mongoose = require('mongoose')
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+}
+
+mongoose.connect(MONGODB_URI, options, () => {
+  console.log('Connected to MongoDB.')
+})
 
 // start express server
 const app = require('./src/app')
