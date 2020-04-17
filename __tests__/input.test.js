cc815a16608d30ba35b8aec34989dca97fa672d8
Eugene Monnier <eugene.monnier@gmail.com>
2020-04-17 09:05:50 -0700

    update client to work with heroku api

--- __tests__/input.test.js
+++ __tests__/input.test.js
@@ -10,8 +10,8 @@ describe('signUp() module', () => {
   describe('Register a new user', () => {
     it('passes a new user to the API', async () => {
       inquirer.prompt = jest.fn().mockResolvedValue({ abc: 'def' })
-      await expect(inquirer.prompt()).resolves.toEqual({ abc: 'def' });
-    });
+      await expect(inquirer.prompt()).resolves.toEqual({ abc: 'def' })
+    })
     it('Properly hashes password before sending to API', async () => {
       mockInput = {
         new_user_email: 'g@g.com',
@@ -23,73 +23,73 @@ describe('signUp() module', () => {
     })
     it('will reject an pre-existing email', () => {
 
-    });
+    })
     it('stores id.txt and jwt.txt', () => {
       // is this test needed
-    });
-  });
-});
+    })
+  })
+})
 describe('signIn() module', () => {
   describe('Sign into account for returning user', () => {
     it('passes sign in data to the API', () => {
       // expect a mock send in the right format
-    });
+    })
     it('will receive existing entries that match the user ID', () => {
 
-    });
-  });
-});
+    })
+  })
+})
 describe('Journals', () => {
   describe('', () => {
 
-  });
-});
+  })
+})
 
 // CRUD
 describe('Create', () => {
   describe('Journals set up module', () => {
     it('passes journal creation to the API', () => {
       // expect properly formatted post
-    });
+    })
     it('checks for journals in the API', () => {
       // expect properly formatted get
-    });
-  });
+    })
+  })
   describe('Entry creation module', () => {
     it('creates a properly formed entry', () => {
 
-    });
+    })
     it('passes the entry to the the API', () => {
 
-    });
-  });
-});
+    })
+  })
+})
 describe('Read', () => {
   describe('Sort by filter module', () => {
     it('should properly sort the entries', () => {
 
-    });
-  });
+    })
+  })
   describe('should render the entry properly', () => {
 
-  });
-});
+  })
+})
 describe('Update', () => {
   describe('updateEntry()', () => {
     it('properly forms the new entry object', () => {
 
-    });
-  });
+    })
+  })
   describe('putApi()', () => {
     it('mocks the object being sent to the API', () => {
 
-    });
-  });
-});
+    })
+  })
+})
 describe('Delete', () => {
   describe('deleteEntry()', () => {
     it('mocks the route to the API for a specific ID of an entry', () => {
-      
-    });
-  });
-});
+
+    })
+  })
+})
